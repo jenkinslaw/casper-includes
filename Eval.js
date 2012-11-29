08c75437ef73b6fb9e201fe14015197a8817f06d
dkinzer <dtkinzer@gmail.com>
2012-11-29 12:18:20 -0500

    Fix broken and add generic menu function.

--- Eval.js
+++ Eval.js
@@ -69,14 +69,13 @@ Eval.dump = function(element) {
  * A Jenkins specific wrapper to title assertions.
  */
 Eval.assertTitle = function(title, message) {
-  title = _add_page_title_boilerplate(title, message);
+  var title = _add_page_title_boilerplate(title, message);
   return t.assertTitle(title);
 }
 
 
 Eval.assertViewExists = function(view) {
   var viewSelector = this.getViewSelector(view);
-  t.assert()
 }
 
 Eval.getViewSelector = function(view) {
@@ -113,13 +112,20 @@ Eval.assertViewContentHasField = function(view, field) {
 
 Eval.getViewFieldSelector = function(view, field) {
   var selector  = this.getViewContentSelector(view);
-  return selector + ' div.views-row-1 div.'
+  return selector + ' div.views-row-1 div.';
 }
 
-Eval.assertViewFieldHasLink = function(view, link) {
+Eval.assertViewFieldHasLink = function(view, field) {
+  var selector = this.getViewFieldLinkSelector(view, field);
+}
+
+Eval.getViewFieldLinkSelector = function (view, field) {
+  var selector = this.getViewFieldSelector('view', 'field');
+  return selector + ' a';
 }
 
 Eval.getViewFieldURL = function (view, field) {
+  var selector = getViewFieldLinkSelector(view, field);
 }
 
 Eval.assertPageHasForm = function(form) {
@@ -134,7 +140,11 @@ Eval.assertBlockTitle = function(block, title) {
 Eval.assertFormHasField = function(form, field) {
 }
 
-Eval.assertSecondaryMenuExists = function(menu) {
+Eval.assertSecondaryMenuExists = function(menu, menu) {
+}
+
+
+Eval.assertSecondaryMenuName = function (menu, name, message) {
 }
 
 Eval.assertPrimaryMenuExists = function(menu) {
