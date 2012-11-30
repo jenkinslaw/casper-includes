39264878c18a6b2a7427cc179b8e4439f8c74fe2
dkinzer <dtkinzer@gmail.com>
2012-11-30 18:24:08 -0500

    qc, Refractor View and Block behaviors from Eval.
    related to jenkinslaw/website#1296

--- Eval.js
+++ Eval.js
@@ -14,8 +14,18 @@ Eval.dump = function(element) {
 }
 
 /**
-* Figures out if there is content given a content selector.
+* A Jenkins specific wrapper to title assertions.
 */
+Eval.assertTitle = function(title, message) {
+  title = _add_page_title_boilerplate(title);
+  return t.assertTitle(title, message);
+}
+
+Eval.loginRequired = function(message){
+  return Eval.assertTitle('Login Required', message);
+}
+
+// Figures out if there is content given a content selector.
 Eval.contentHasItems = function(selector) {
   return $(selector).length >= 1;
 }
@@ -54,110 +64,7 @@ Eval.getHref = function(selector) {
   }, Arguments);
 }
 
-/**
-* Fetch the text of the first found of given selector.
-*/
-Eval.fetchFirstText = function(selector) {
-  var Arguments = {
-    'selector' : selector
-  };
-
-  return casper.evaluate(function(selector){
-    return $(selector + ':first').text();
-  }, Arguments);
-}
-
-/**
-* A Jenkins specific wrapper to title assertions.
-*/
-Eval.assertTitle = function(title, message) {
-  title = _add_page_title_boilerplate(title);
-  return t.assertTitle(title, message);
-}
-
-Eval.loginRequired = function(message){
-  return Eval.assertTitle('Login Required', message);
-}
-
-Eval.assertViewExists = function(view, message) {
-  var selector = this.getViewSelector(view);
-  return t.assertSelectorExists(selector, message);
-}
-
-Eval.getViewSelector = function(view) {
-  if (this.isBlockView(view)) {
-    return this.getBlockViewSelector(view);
-  }
-  else {
-    return 'div.view.view-' + view;
-  }
-}
-
-Eval.getBlockViewSelector = function(view) {
- if (typeof view.id != 'undefined' && typeof view.label != 'undefined') {
-   var selector = 'views-' + view.id + '-' + view.block;
- }
-  return this.getBlockSelector(selector);
-}
-
-Eval.assertBlockHasView = function(block, view) {
-}
-
-Eval.isBlockView = function(view) {
-  return (typeof view.block != "undefined");
-}
-
-Eval.assertViewHasContent = function(view, message) {
-  var selector = this.getViewFirstRowSelector(view);
-  this.dump(selector);
-  return t.assertSelectorExists(selector, message);
-}
-
-Eval.getViewFirstRowSelector = function(view) {
-  var selector = this.getViewContentSelector(view);
-  return selector + ' .views-row-first';
-}
-
-Eval.getViewContentSelector = function(view) {
-  var selector  = this.getViewSelector(view);
-  return selector + ' div.view-content';
-}
-
-Eval.assertViewContentHasField = function(view, field) {
-  var selector = this.getViewFieldSelector(view, field);
-}
-
-Eval.getViewFieldSelector = function(view, field) {
-  var selector  = this.getViewContentSelector(view);
-  return selector + ' div.views-row-1 div.' + field;
-}
-
-Eval.assertViewFieldHasLink = function(view, field) {
-  var selector = this.getViewFieldLinkSelector(view, field);
-}
-
-Eval.getViewFieldLinkSelector = function (view, field) {
-  var selector = this.getViewFieldSelector('view', 'field');
-  return selector + ' a';
-}
-
-Eval.getViewFieldURL = function (view, field) {
-  var selector = getViewFieldLinkSelector(view, field);
-}
-
 Eval.assertPageHasForm = function(form) {
-}
-
-Eval.assertBlockExists = function(block, message) {
-  var selector = this.getBlockSelector(block);
-  return t.assertSelectorExists(selector, message);
-}
-
-Eval.getBlockSelector = function(block) {
-  return 'div#block-' + block;
-}
-
-Eval.assertBlockTitle = function(block, title) {
 }
 
 Eval.assertFormHasField = function(form, field) {
@@ -180,5 +87,122 @@ Eval.assertSecondaryMenuName = function (menu, name, message) {
 }
 
 Eval.assertPrimaryMenuExists = function(menu) {
+}
+
+// Fetch the text of the first found of given selector.
+Eval.fetchFirstText = function(selector) {
+  var Arguments = {
+    'selector' : selector
+  };
+
+  return casper.evaluate(function(selector){
+    return $(selector + ':first').text();
+  }, Arguments);
+}
+
+/**
+ * Define Drupal Block and casper behaviors.
+ */
+Block = function(id, title) {
+  var defaults = {
+    id : '',
+    title : ''
+  };
+
+  this.selector = 'div#block-';
+  this.id = (typeof id === 'undefined') ? defaults.id : id;
+  this.title = (typeof title === 'undefined') ? defaults.title : title;
+}
+
+Block.assertExists = function() {
+  var selector = this.getBlockSelector(block);
+  return t.assertSelectorExists(selector, message);
+}
+
+Block.assertHasView = function(view) {
+}
+
+Block.getSelector = function(block_id) {
+  var id = (typeof block_id === 'undefined') ? this.id : block_id;
+  return this.selector + this.id;
+}
+
+Block.assertTitle = function(title) {
+}
+
+
+/**
+ * Define Drupal View and casper behaviours.
+ */
+View = function(id, display, fields, items, isBox, isTable){
+  var defaults = {
+    fields : {},
+    items : {},
+    display : '',
+    isBox : false,
+    isTable : true
+  };
+
+  this.id = id;
+  this.fields = (typeof fields === 'undefined') ? defaults.fields : fields;
+  this.items = (typeof items === 'undefined') ? defaults.fields : fields;
+  this.isBox = (typeof isBox == 'undefined') ? defaults.isBox : isBox;
+  this.isTable = (typeof isTable == 'undefined') ? defaults.isTable : isTable;
+}
+
+View.getBlockSelector = function() {
+  var selector = 'views-' + view.id + '-' + this.display;
+  return Block.getSelector(selector);
+}
+
+View.assertExists = function(view, message) {
+  var selector = this.getSelector();
+  return t.assertSelectorExists(selector, message);
+}
+
+View.getSelector = function(view) {
+  if (this.isBlock) {
+    return this.getBlockViewSelector(view);
+  }
+  else {
+    return 'div.view.view-' + view;
+  }
+}
+
+View.assertHasContent = function(view, message) {
+  var selector = this.getFirstRowSelector(view);
+  return t.assertSelectorExists(selector, message);
+}
+
+View.getFirstRowSelector = function(view) {
+  var selector = this.getContentSelector(view);
+  return selector + ' .views-row-first';
+}
+
+View.getContentSelector = function() {
+  var selector  = this.getSelector();
+  return selector + ' div.view-content';
+}
+
+View.assertContentHasField = function(field) {
+  var selector = this.getFieldSelector(field);
+}
+
+View.getFieldSelector = function(field) {
+  var selector  = this.getContentSelector(view);
+  return selector + ' div.views-row-1 div.' + field;
+}
+
+View.assertFieldHasLink = function(field) {
+  var selector = this.getFieldLinkSelector(field);
+}
+
+View.getFieldLinkSelector = function (field) {
+  var selector = this.getFieldSelector('view', 'field');
+  return selector + ' a';
+}
+
+View.getFieldURL = function (view, field) {
+  var selector = this.getFieldLinkSelector(view, field);
 }
 
