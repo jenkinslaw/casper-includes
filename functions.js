4ea52e1ea11d18e55cb34c01bec8cf18a74b3ce0
David Kinzer <dtkinzer@gmail.com>
2013-08-08 01:25:52 +0000

    Do not wrap functions, and use strict.

--- functions.js
+++ functions.js
@@ -1,38 +1,41 @@
-(function(){
+function _login_user(casper, username){
   "use strict";
-  function _login_user(casper, username){
-    var user = _get_roleuser(casper, username);
-    _fill_submit_login_form(casper, user);
-  }
+  var user = _get_roleuser(casper, username);
+  _fill_submit_login_form(casper, user);
+}
 
-  function _get_roleuser(casper, username) {
-    username = _roleuser_name_lower(username);
-    var roleusers = casper.getGlobal('Drupal').settings.roleusers;
-    var user = roleusers[username];
-    return user;
-  }
+function _get_roleuser(casper, username) {
+  "use strict";
+  username = _roleuser_name_lower(username);
+  var roleusers = casper.getGlobal('Drupal').settings.roleusers;
+  var user = roleusers[username];
+  return user;
+}
 
-  function _fill_submit_login_form(casper, user_object){
-    casper.fill('#user-login-form',{
-      'name' : user_object.name,
-      'pass' : user_object.pass,
-      'pass-fake' : user_object.pass
-    }, true
-    );
-  }
+function _fill_submit_login_form(casper, user_object){
+  "use strict";
+  casper.fill('#user-login-form',{
+    'name' : user_object.name,
+    'pass' : user_object.pass,
+    'pass-fake' : user_object.pass
+  }, true
+  );
+}
 
-  function _logout_user(casper) {
-    var logout_selector = ".wb-logout-link";
-    casper.click(logout_selector);
-  }
+function _logout_user(casper) {
+  "use strict";
+  var logout_selector = ".wb-logout-link";
+  casper.click(logout_selector);
+}
 
-  function _roleuser_name_lower(username) {
-    username = username.toLowerCase();
-    return username;
-  }
+function _roleuser_name_lower(username) {
+  "use strict";
+  username = username.toLowerCase();
+  return username;
+}
 
-  function _add_page_title_boilerplate(title) {
-    title += " | Jenkins Law Library";
-    return title;
-  }
-}());
+function _add_page_title_boilerplate(title) {
+  "use strict";
+  title += " | Jenkins Law Library";
+  return title;
+}
